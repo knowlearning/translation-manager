2a2d3f53165bb24e02614033b796bd4a04121e59
Jason Ford <fordjason@gmail.com>
2024-11-22 13:02:09 -0800

    Add edit interface

--- src/translatable-targets.vue
+++ src/translatable-targets.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { computed, ref, reactive, watch } from 'vue'
+  import { reactive, ref, watch } from 'vue'
   import languageCodes from './language-codes.js'
 
   const TRANSLATION_DOMAIN = 'f74e9cb3-2b53-4c85-9b0c-f1d61b032b3f.localhost:5889'
@@ -13,6 +13,9 @@
   const currentEditTranslation = ref(null)
   const headers = ref([])
   const items = ref([])
+  const editing = ref(false)
+  const edits = reactive({})
+  const openEditor = ref(null)
 
   await loadTranslations()
 
@@ -25,22 +28,25 @@
     const translations = await Agent.query('translations-for-item', [id, languages.value], TRANSLATION_DOMAIN)
 
     console.log('translations-for-item', translations)
-    const languagePlaceholders = (
-      languages
-        .value
-        .reduce((acc, language) => {
-          acc[language] = false
-          return acc
-        }, {})
-    )
+    const languagePlaceholders = Object.fromEntries(languages.value.map(k => [k, null]))
+
+    let sourceLanguage
 
     const t = (
       translations
         .reduce((acc, { translatable_target, language, path, is_source, value }) => {
-          if (!acc[translatable_target]) acc[translatable_target] = { path: path.slice(1), ...languagePlaceholders }
-          if (is_source) acc[translatable_target].source = value
+          if (!acc[translatable_target]) {
+            acc[translatable_target] = {
+              path: path.slice(1),
+              ...languagePlaceholders
+            }
+          }
+          if (is_source) {
+            acc[translatable_target].source = value
+            sourceLanguage = language
+          }
 
-          acc[translatable_target][language] = true
+          acc[translatable_target][language] = value
 
           return acc
         }, {})
@@ -48,11 +54,26 @@
 
     console.log(t)
 
-
-
-
-    headers.value = ['path', ...languages.value]
+    headers.value = [
+      {
+        title: 'path', //  TODO: every path element should get its header
+        key: 'path'
+      },
+      {
+        title: `${sourceLanguage} (source)`,
+        key: 'source'
+      },
+      ...languages
+        .value
+        .filter(language => language !== 'source' && language !== sourceLanguage)
+        .map(language => ({
+          title: language,
+          key: language
+        }))
+    ]
     items.value = Object.values(t)
+    console.log('headers', headers.value)
+    console.log('items', items.value)
   }
 
   async function loadTranslationEdits() {
@@ -75,26 +96,73 @@
     console.log('edit data...', editData)
   }
 
+  function editKey({ path }, language) { return JSON.stringify([...path, language]) }
+
 </script>
 
 <template>
   <div>
     <h1>Translations</h1>
-    <v-select
-      v-model="languages"
-      label="Select languages to show"
-      multiple
-      chips
-      closable-chips
-      :headers="headers"
-      :items="languageCodes.map(({ code, name }) => ({ title: name, subtitle: code, value: code }))"
-    />
     <v-container>
+      <v-select
+        v-model="languages"
+        label="Select languages to show"
+        multiple
+        chips
+        closable-chips
+        :items="languageCodes.map(({ code, name }) => ({ title: name, subtitle: code, value: code }))"
+      />
+      <v-switch
+        v-model="editing"
+        color="primary"
+        :label="editing ? 'Editing' : 'Edit'"
+      />
       <v-data-table
         sticky
+        :headers="headers"
         :items="items"
-        show-select
+        show-slect
       >
+        <template v-slot:item.path="{ value }">
+          {{ value }}
+        </template>
+        <template v-slot:item.source="{ value }">
+          {{ value }}
+        </template>
+        <template
+          v-for="language in languages"
+          v-slot:[`item.${language}`]="{ value, item }"
+        >
+          <div
+            v-if="editing && openEditor === editKey(item, language)"
+          >
+            <v-textarea
+              variant="outlined"
+              autofocus
+              v-model="edits[editKey(item, language)]"
+            />
+            <v-btn
+              text="cancel"
+              @click="openEditor = null"
+            />
+            <v-btn
+              text="save"
+              @click="() => {
+                openEditor = null
+              }"
+            />
+          </div>
+          <div v-else>
+            {{ value }}
+            <v-btn
+              v-if="editing && !openEditor"
+              variant="plain"
+              size="x-small"
+              icon="fa fa-pencil"
+              @click="openEditor = editKey(item, language)"
+            />
+          </div>
+        </template>
       </v-data-table>
     </v-container>
   </div>
